2c3bd29bab720bf2806d4067aed9cb958cf2b9c1
michael.desmedt <michael.desmedt@icloud.com>
2025-04-19 02:32:25 +0200

    Basic server listening.

---
+++ server/build.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // This is a build script that runs before the main program.
+    // It is used to compile the protobuf files into Rust code.
+    // The generated code will be placed in the src/protos directory.
+    println!("cargo:rerun-if-changed=../core/serialization/protos");
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // Compile the protobuf files
+    prost_build::compile_protos(
+        &["../core/serialization/protos/hello.proto"],
+        &["../core/serialization/protos"],
+    )
+    .unwrap_or_else(|e| panic!("Failed to compile protos: {}", e));
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+include!(concat!(env!("OUT_DIR"), "/core.serialization.protos.rs"));
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    // Open a listener on the specified address
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:4242")
+        .await
+        .unwrap();
+    println!("Listening on 127.0.0.1:4242");
+
+    loop {
+        // Accept a new connection
+        let (socket, _) = listener.accept().await.unwrap();
+        println!("Accepted connection from {:?}", socket);
+    }
 }
