f52b20b583244509fb40943e4408422f68159a6d
michael.desmedt <michael.desmedt@icloud.com>
2025-04-22 00:46:10 +0200

    * Réception et Traitement du message HelloServeur [sc-87] * Envoi du Message HelloClient [sc-88] * Réception et Traitement de HelloClient [sc-89]

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,42 +1,72 @@
 use prost::Message;
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 use void_architect_core::serialization::protos;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    // Initialize the logger
+    env_logger::Builder::new()
+        .filter_level(log::LevelFilter::max())
+        .init();
 
     let mut stream = match TcpStream::connect("127.0.0.1:4242").await {
         Ok(stream) => stream,
         Err(e) => {
-            println!("Failed to connect: {e}");
+            log::error!("Failed to connect: {e}");
             return;
         }
     };
 
-    println!("Connected to server");
+    log::info!("Connected to server");
 
-    loop {
-        // Wait for the soket to be ready for writing
-        stream.writable().await.unwrap();
-        let hello_server = protos::HelloServer {
-            client_info: "4242".to_string(),
-        };
+    // Wait for the soket to be ready for writing
+    stream.writable().await.unwrap();
+    let hello_server = protos::HelloServer {
+        client_info: "Hello!".to_string(),
+    };
 
-        let buffer: Vec<u8> = hello_server.encode_to_vec();
-        match AsyncWriteExt::write_all(&mut stream, &buffer).await {
-            Ok(_) => {
-                println!("Sent: {:?}", hello_server);
-            }
-            Err(e) => {
-                println!("Failed to send: {e}");
-                break;
-            }
+    let buffer: Vec<u8> = hello_server.encode_to_vec();
+    match AsyncWriteExt::write_all(&mut stream, &buffer).await {
+        Ok(_) => {
+            log::info!("Sent: {:?}", hello_server);
         }
+        Err(e) => {
+            log::error!("Failed to send: {e}");
+            return;
+        }
+    }
 
-        break;
+    // Wait for the socket to be ready for reading
+    stream.readable().await.unwrap();
+    let mut buffer = [0; 1024];
+    match stream.read(&mut buffer).await {
+        Ok(n) => {
+            if n == 0 {
+                log::info!("Server disconnected");
+                return;
+            }
+            log::info!("Received {n} bytes from server");
+            // Decode the message
+            let hello_client: protos::HelloClient = match protos::HelloClient::decode(&buffer[..n])
+            {
+                Ok(hello_client) => hello_client,
+                Err(e) => {
+                    log::error!("Failed to decode message: {e}");
+                    return;
+                }
+            };
+
+            log::info!("Received: {:?}", hello_client);
+        }
+        Err(e) => {
+            log::error!("Failed to read from stream: {e}");
+            return;
+        }
     }
 
     stream.shutdown().await.unwrap();
-    println!("Closing client");
+    log::info!("Closing client");
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,10 @@
 use env_logger::Builder;
 use log::LevelFilter;
-use tokio::net::TcpListener;
+use prost::Message;
+use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener};
 use void_architect_core::serialization::protos;
+
+const MAX_MESSAGE_SIZE: usize = 1024;
 
 fn get_config() -> (String, String) {
     // TODO: Load the configuration from environment variables or a config file
@@ -55,7 +58,76 @@ async fn main() {
 
     loop {
         // Accept a new connection
-        let (socket, _) = listener.accept().await.unwrap();
-        log::info!("Accepted connection from {:#?}", socket);
+        let (stream, _) = listener.accept().await.unwrap();
+        log::info!("Accepted connection from {:#?}", stream.peer_addr());
+
+        // Swap a new task to handle the connection
+        tokio::spawn(handle_connection(stream));
+    }
+}
+
+async fn handle_connection(mut stream: tokio::net::TcpStream) {
+    // The current handshade protocol is a simple ping-pong between
+    // the client and the server. The client need to send a HelloServer
+    // message and the server will reply with a HelloClient message.
+    log::info!("Waiting for client...");
+    let hello_server: protos::HelloServer;
+    match stream.readable().await {
+        Ok(_) => {
+            let mut buffer = [0; MAX_MESSAGE_SIZE];
+            // Wait for the socket to be ready for reading
+            match stream.read(&mut buffer).await {
+                Ok(n) => {
+                    if n == 0 {
+                        log::info!("Client disconnected");
+                        return;
+                    }
+                    log::info!("Received {n} bytes from client");
+
+                    // Decode the message
+                    hello_server = match protos::HelloServer::decode(&buffer[..n]) {
+                        Ok(hello_server) => hello_server,
+                        Err(e) => {
+                            log::error!("Failed to decode message: {e:#?}");
+                            return;
+                        }
+                    };
+                    log::info!("Received: {hello_server:#?}");
+                }
+                Err(e) => {
+                    log::error!("Failed to read from stream: {e:#?}");
+                    return;
+                }
+            }
+        }
+        Err(e) => {
+            log::error!("Failed to read from stream: {e:#?}");
+            return;
+        }
+    }
+
+    // Send a HelloServer message back to the client
+    let hello_client = protos::HelloClient {
+        server_info: hello_server.client_info,
+    };
+    let message = hello_client.encode_to_vec();
+    // Waiting for the socket to be ready for writing
+    match stream.writable().await {
+        Ok(_) => {
+            log::info!("Sending message to client");
+            match stream.write_all(&message).await {
+                Ok(_) => {
+                    log::info!("Sent: {hello_client:#?}");
+                }
+                Err(e) => {
+                    log::error!("Failed to send message: {e:#?}");
+                    return;
+                }
+            }
+        }
+        Err(e) => {
+            log::error!("Failed to write to stream: {e:#?}");
+            return;
+        }
     }
 }
