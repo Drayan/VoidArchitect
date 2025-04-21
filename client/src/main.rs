20a23ef11bbc24a7fcdbf23c32147e477dd1f233
michael.desmedt <michael.desmedt@icloud.com>
2025-04-21 19:34:07 +0200

    Connexion TCP Asynchrone au Serveur [sc-85]

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,18 @@
+use tokio::net::TcpStream;
 use void_architect_core::serialization::protos;
-fn main() {
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let mut stream = match TcpStream::connect("127.0.0.1:4242").await {
+        Ok(stream) => stream,
+        Err(e) => {
+            println!("Failed to connect: {e}");
+            return;
+        }
+    };
+
+    println!("Connected to server");
+    println!("Stream: {stream:#?}");
 }
