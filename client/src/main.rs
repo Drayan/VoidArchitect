eec10293dcebdbba77f047b6209c0e0e7f3fc577
michael.desmedt <michael.desmedt@icloud.com>
2025-04-21 20:16:03 +0200

    Envoi du Message HelloServer [sc-86]

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,5 @@
-use tokio::net::TcpStream;
+use prost::Message;
+use tokio::{io::AsyncWriteExt, net::TcpStream};
 use void_architect_core::serialization::protos;
 
 #[tokio::main]
@@ -14,5 +15,28 @@ async fn main() {
     };
 
     println!("Connected to server");
-    println!("Stream: {stream:#?}");
+
+    loop {
+        // Wait for the soket to be ready for writing
+        stream.writable().await.unwrap();
+        let hello_client = protos::HelloClient {
+            server_info: "4242".to_string(),
+        };
+
+        let buffer: Vec<u8> = hello_client.encode_to_vec();
+        match AsyncWriteExt::write_all(&mut stream, &buffer).await {
+            Ok(_) => {
+                println!("Sent: {:?}", hello_client);
+            }
+            Err(e) => {
+                println!("Failed to send: {e}");
+                break;
+            }
+        }
+
+        break;
+    }
+
+    stream.shutdown().await.unwrap();
+    println!("Closing client");
 }
